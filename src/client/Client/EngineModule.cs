3f3acd1f0e67710a019c5d580dd2540c3e16416c
fdsprod <fdsprod@gmail.com>
2011-10-26 22:58:27 +0000

    Added Any CPU to Solution.

--- src/client/Client/EngineModule.cs
+++ src/client/Client/EngineModule.cs
@@ -14,6 +14,7 @@
  *   (at your option) any later version.
  ***************************************************************************/
 
+
 using Client.Configuration;
 using Client.Diagnostics;
 using Client.Modules;
